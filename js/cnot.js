763b119cab08c71ea4710b2a6a2b5eac3428c3d3
Conor Power <conor.de-paor@ucdconnect.ie>
2021-02-07 15:40:15 +0000

    First draft of cnot

---
+++ js/cnot.js
@@ -0,0 +1,7 @@
+function cnot_init(){
+    var cnot_gist = document.getElementById('cnot-gist');
+    var gist_script = document.createElement("script");
+
+    //gist_script.src = "https://gist.github.com/conorp854/1c58e183d153804deaf75f214873049b.js";
+    //cnot_gist.appendChild(gist_script);
+}
--- js/contentLoad.js
+++ js/contentLoad.js
@@ -2,28 +2,37 @@
 //To be implemented
 var HOME_HASH = ""
 var HASH2URL  = {};
+var HASH2FUNC = {
+    "projects/projects_quant_cnot" : cnot_init
+};
 
 //Content Serving
-async function fetchHtmlAsText(url) {
+async function fetchAsText(url) {
     return await (await fetch(url)).text();
 }
 
-async function loadtext(url) {
+async function loadtext(url,script_init) {
     const contentDiv = document.getElementById("content-index");
-    contentDiv.innerHTML = await fetchHtmlAsText(url);
+    contentDiv.innerHTML = await fetchAsText(url);
+    if(script_init){
+        console.log("running script")
+        script_init()
+    }
+    renderMathInElement(document.body);//Load math on page now after loading page
 }
 
-function loadPage(hash) {
+function loadPage(hash,script_init) {
     if (hash !== '') {
-        loadtext(hash.concat(".html"));
+        loadtext(hash.concat(".html"),script_init);
     } else {
-        loadtext("home".concat(".html"));
+        loadtext("home".concat(".html"),script_init);
     }
 }
 
 //Routing Hash Function
 window.onhashchange = function() { //Must assign a function to onhashchange
     hash = window.location.hash.substring(1); //Puts hash in variable, and removes the # character
-    loadPage(hash);
+    script_init = HASH2FUNC[hash];
+    loadPage(hash,script_init);
 }
 window.onhashchange();
