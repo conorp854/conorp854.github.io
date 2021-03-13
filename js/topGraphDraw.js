4c4a88ddccfb650311dbe6df8a32f8221bb7b8ac
Conor Power <conor.de-paor@ucdconnect.ie>
2021-03-13 13:37:43 +0000

    Interactive sinc animation in header

---
+++ js/topGraphDraw.js
@@ -0,0 +1,65 @@
+function plotSinc(ctx, xOffset, step, scale) {
+    var width = ctx.canvas.width;
+    var height = ctx.canvas.height;
+
+    ctx.beginPath();
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = "rgb(124,0,31)";
+
+    var x = 0.1*scale;
+    var y = 0;
+    var freq_sinc = 0.08;
+    var freq_amp  = 0.03;
+    var amplitude = 50*Math.sin(freq_amp*step);
+    while (x < width) {
+        y = height/2 + amplitude*Math.sin(freq_sinc*(x - (width/2 + xOffset)))/(freq_sinc*(x - (width/2 + xOffset)));
+        ctx.lineTo(x, y);
+        x+=scale;
+    }
+    ctx.stroke();
+    ctx.save();
+}
+
+function fitToContainer(canvas, scale){
+    // Make it visually fill the positioned parent
+    canvas.style.width ='100%';
+    canvas.style.height='100%';
+    // ...then set the internal size to match
+    canvas.width  = canvas.offsetWidth*scale;
+    canvas.height = canvas.offsetHeight*scale;
+  }
+
+function draw() {
+    var canvas = document.getElementById("topGraph");
+    var context = canvas.getContext("2d");
+    var scale = 2;
+    fitToContainer(canvas, scale);
+
+    canvas.onclick = function (e) {
+        // e is the mouse click event
+        var rect = e.target.getBoundingClientRect();
+        var x = (e.clientX - rect.left - canvas.width/4)*scale; //x position within the element relative to centre.
+        mousePosition.x = x;
+    }
+
+    //Default State with just axes to restore to
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.save();            
+    
+    //Restore to empty axes after current sine iteration
+    plotSinc(context, mousePosition.x, step, scale);
+    context.restore();
+    
+    step += 1;
+    window.requestAnimationFrame(draw);
+}
+
+function initDraw() {
+    window.requestAnimationFrame(function() {
+        draw();
+    });
+    draw();
+}
+var mousePosition = {x: 0, y:0};
+var step = 0;
+initDraw();
